d5f0de2572955e617c49057d6f1727d754ce29fd
Homa Wong <homawong@gmail.com>
2016-04-04 12:44:51 -0700

    Add support for bower

--- generators/beta/index.js
+++ generators/beta/index.js
@@ -17,8 +17,15 @@ const changeCase = require('change-case');
 //   }
 // });
 
+const collectingInfo = [];
+
 module.exports = yeoman.Base.extend({
   initializing: {
+    initProps() {
+      this.props = {
+        source: {}
+      };
+    },
     loadRepo() {
       //     const done = this.async();
       //     NodeGit.Repository.open(path.resolve('.')).then((repo) => {
@@ -32,33 +39,149 @@ module.exports = yeoman.Base.extend({
     }
   },
   prompting: {
-    greeting() {
-      this.log(yosay(`Welcome to the sensational ${chalk.yellow('typings')} generator!`));
-    },
-    askSource() {
+    betaGreeting() {
       this.log('Welcome to the beta! Let me know if my questions make sense to you.');
       this.log('Now, let\'s get started...');
       this.log('');
-      const done = this.async();
-      this.log(`To begin, I need to know a little bit about the ${chalk.green('source')} you are typings for.`);
+    },
+    greeting() {
+      this.log(yosay(`Welcome to the sensational ${chalk.yellow('typings')} generator!`));
       this.log('');
-      this.log('Beta note: some of these questions will be skipped in actual release');
-      this.log('  if I can access the source and determine them myself.');
-
+      this.log(`To begin, I need to know a little bit about the ${chalk.green('source')} you are typings for.`);
+    },
+    askDelivery() {
       const questions = [
         {
           type: 'list',
-          name: 'delivery',
-          message: `How can I ${chalk.green('install')} it?`,
+          name: 'type',
+          message: `Where can I get it ${chalk.green('from')}?`,
           choices: [
-            { name: 'Bower', value: 'bower', disabled: 'coming soon...' },
-            { name: 'Duo', value: 'duo', disabled: 'coming not so soon...' },
-            { name: 'Jam', value: 'jam', disabled: 'coming not so soon...' },
-            { name: 'JSPM', value: 'jspm', disabled: 'coming not so soon...' },
-            { name: 'NPM', value: 'npm', checked: true },
-            { name: 'volo', value: 'volo', disabled: 'coming not so soon...' },
-          ]
+            { name: 'Bower', value: 'bower' },
+            { name: 'CDN or http(s)', value: 'http' },
+            // { name: 'Duo', value: 'duo', disabled: 'coming not so soon...' },
+            // { name: 'Jam', value: 'jam', disabled: 'coming not so soon...' },
+            // { name: 'JSPM', value: 'jspm', disabled: 'coming not so soon...' },
+            { name: 'NPM', value: 'npm' },
+            // { name: 'volo', value: 'volo', disabled: 'coming not so soon...' },
+            { name: 'cannot be downloaded', value: 'none' },
+          ],
+          default: 'npm'
         },
+        {
+          type: 'input',
+          name: 'name',
+          message: (props) => {
+            switch (props.type) {
+              case 'http':
+              case 'none':
+                return `What is the ${chalk.green('name')} of the package?`;
+              default:
+                return `${chalk.cyan(props.type)} install ${chalk.green('<package name>')}?`;
+            }
+          },
+          validate: (value) => value.length > 0,
+        },
+        {
+          type: 'input',
+          name: 'url',
+          message: `What is the ${chalk.green('url')} of the package?`,
+          validate: (value) => value.length > 0,
+          when: (props) => props.type === 'http',
+        },
+      ];
+
+      const done = this.async();
+      this.prompt(questions, (props) => {
+        this.props.source.delivery = props;
+        done();
+      });
+    },
+    getInfoFromDelivery() {
+      const delivery = this.props.source.delivery;
+      if (delivery.type !== 'none') {
+        this.log(`gathering info from ${chalk.cyan(delivery.type)}...`);
+      }
+
+      switch (delivery.type) {
+        case 'http':
+        case 'none':
+          this.props.source.delivery.main = 'index';
+
+          const done = this.sync();
+          this.prompts([
+            {
+              type: 'input',
+              name: 'version',
+              message: `What is the ${chalk.green('version')} of the package?`,
+              validate: (value) => value.length > 0,
+              when: (props) => ['http', 'none'].indexOf(props.type) !== -1,
+            },
+            {
+              type: 'input',
+              name: 'homepage',
+              message: `Enter the ${chalk.green('homepage')} of the package (if any)`,
+            },
+          ], (props) => {
+            this.props.source.version = props.version;
+            this.props.source.homepage = props.homepage;
+            done();
+          });
+          break;
+        case 'bower':
+          collectingInfo.push(new Promise((resolve, reject) => {
+            const child = this.spawnCommand('bower', ['info', delivery.name, '--json'], { stdio: [0, 'pipe'] });
+            child.on('close', (code) => {
+              if (code !== 0) {
+                reject(`${chalk.red('Oops')}, could not find ${chalk.cyan(delivery.name)}.`);
+              }
+            });
+
+            child.stderr.on('data', (data) => {
+              try {
+                const result = JSON.parse(data.toString());
+                if (result.id === 'validate') {
+                  this.props.source.repository = result.data.pkgMeta._source;
+                }
+              }
+              catch (err) { }
+            });
+            child.stdout.on('data', (data) => {
+              const result = JSON.parse(data.toString());
+              this.props.source.delivery.main = 'index';
+              this.props.source.version = result.latest.version;
+              this.props.source.homepage = result.latest.homepage;
+              resolve();
+            });
+          }));
+          break;
+        case 'npm':
+          collectingInfo.push(new Promise((resolve, reject) => {
+            const child = this.spawnCommand('npm', ['info', delivery.name, '--json'], { stdio: [0, 'pipe'] });
+            child.on('close', (code) => {
+              if (code !== 0) {
+                reject(`${chalk.red('Oops')}, could not find ${chalk.cyan(delivery.name)}.`);
+              }
+            });
+
+            child.stdout.on('data', (data) => {
+              console.log('stdout', data);
+              const pjson = JSON.parse(data.toString());
+              this.props.source.delivery.main = path.parse(pjson.main).name;
+              this.props.source.version = pjson.version;
+              this.props.source.homepage = pjson.homepage;
+              this.props.source.repository = pjson.repository && pjson.repository.url ?
+                pjson.repository.url : pjson.repository;
+              resolve();
+            });
+          }));
+          break;
+      }
+    },
+    askUsage() {
+
+    },
+    askSource() {
+      const other = [
         {
           type: 'checkbox',
           name: 'kinds',
@@ -69,7 +192,7 @@ module.exports = yeoman.Base.extend({
               {
                 name: 'github (Duo, JSPM, volo, etc)',
                 value: 'github',
-                checked: props.delivery === 'duo' || props.delivery === 'jspm' ||props.delivery === 'volo'
+                checked: props.delivery === 'duo' || props.delivery === 'jspm' || props.delivery === 'volo'
               },
               { name: 'Bower', value: 'bower', checked: props.delivery === 'bower' },
               { name: 'Jam', value: 'jam', checked: props.delivery === 'jam' },
@@ -77,14 +200,6 @@ module.exports = yeoman.Base.extend({
               { name: 'Platform (e.g. atom)', value: 'env' },
             ]
           }
-        },
-        {
-          type: 'input',
-          name: 'npmName',
-          when: (props) => props.packageManagers.indexOf('npm') !== -1,
-          message: `What is the ${chalk.green('package name')} in ${chalk.cyan('NPM')}?`,
-          default: (props) => props.repository,
-          validate: (value) => value.length > 0,
         },
         {
           type: 'input',
@@ -167,15 +282,30 @@ module.exports = yeoman.Base.extend({
         },
       ];
 
-      this.prompt(questions, (props) => {
-        this.source = props;
-        console.log(props);
+      // this.prompt(questions, (props) => {
+      //   this.source = props;
+      //   console.log(props);
+      //   done();
+      // });
+    },
+  },
+  install: {
+    waitForCollectingInfo() {
+      const done = this.async();
+      Promise.all(collectingInfo).then(() => {
         done();
+      }, (err) => {
+        this.log(err);
+        process.exit(1);
       });
     },
+    printProps() {
+      this.log(this.props);
+    }
   },
   end: {
     sayGoodbye() {
+      this.log('');
       this.log('That\'s it for the Beta right now. Thanks for trying it out!');
       this.log('');
       this.log('If you have any suggestion, please create an issue at:');
